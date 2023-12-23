c0fdb0be15086b2f6c0d534af819a4aafbf4ab98
Liam582004 <hohungloc58@gmail.com>
2023-12-23 16:40:33 +0700

    push detail

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // FontAwesome Icons
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 
 //Material UI Config
-import materialConfig from './config/material.config';
-import { ThemeProvider } from '@mui/material/styles';
+import materialConfig from "./config/material.config";
+import { ThemeProvider } from "@mui/material/styles";
 
 // Import Components
-import Home from './modules/home/pages/Home';
+import Home from "./modules/home/pages/Home";
 
 //Import tailwind css
-import './styles/tailwind.css';
+import "./styles/tailwind.css";
 
-import MainLayout from './modules/layout/pages/MainLayout';
-import NotFound from './Components/NotFound/NotFound';
-import User from './modules/user/pages/User';
-import Login from './modules/user/components/Login';
-import SignUp from './modules/user/components/SignUp';
+import MainLayout from "./modules/layout/pages/MainLayout";
+import NotFound from "./Components/NotFound/NotFound";
+import User from "./modules/user/pages/User";
+import Login from "./modules/user/components/Login";
+import SignUp from "./modules/user/components/SignUp";
+import Detail from "./modules/details/pages/Detail";
 
 // Thêm icon vào thư viện
 library.add(fas, far);
@@ -31,6 +32,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainLayout />}>
             <Route index element={<Home />} />
+            <Route path="/details/:moveId" element={<Detail />} />
             <Route path="user" element={<User />}>
               <Route path="login" element={<Login />} />
               <Route path="signup" element={<SignUp />} />
