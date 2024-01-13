705d51e0e4ec74660acc858f5591709fd868748d
smoluca123 <lluca8257@gmail.com>
2024-01-13 11:25:48 +0700

    update, complete

--- src/modules/layout/pages/AdminLayout.jsx
+++ src/modules/layout/pages/AdminLayout.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navigate from '../../admin/components/Navigate';
 import { Header } from '../../../Components/Header';
+import { useSelector } from 'react-redux';
+import { Loading } from '../../../Components/Loading';
 export default function AdminLayout() {
+  const { isLoading } = useSelector((state) => state.loading);
   return (
     <div>
+      {/* Show loading */}
+      {isLoading && <Loading />}
       <Header />
       <div className="pt-[60px]">
         <Navigate />
