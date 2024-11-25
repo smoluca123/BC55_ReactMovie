3c44cb0be0989eea16abe3e4d1daf72b07edc06e
Luca Nguyen <108933241+smoluca123@users.noreply.github.com>
2024-11-25 14:45:35 +0700

    Update Token baseAPI.js

--- src/apis/baseAPI.js
+++ src/apis/baseAPI.js
@@ -7,7 +7,7 @@ const baseAPI = axios.create({
   },
   headers: {
     TokenCybersoft:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1NSIsIkhldEhhblN0cmluZyI6IjI0LzA1LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxNjUwODgwMDAwMCIsIm5iZiI6MTY4Nzg4NTIwMCwiZXhwIjoxNzE2NjU2NDAwfQ.HsoestvkIN5Kub3jnAr8UddrPugJcxCsAm4QfMi4ZbU',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1NSIsIkhldEhhblN0cmluZyI6IjI0LzA1LzIyMjIiLCJIZXRIYW5UaW1lIjoiMTcxNjUwODgwMDAwMCIsIm5iZiI6MTY4Nzg4NTIwMCwiZXhwIjoxNzE2NjU2NDAwfQ._zGig3qRiiBsp3ISyCdJnpwe6erHcB-KUpka3g0VMPE',
   },
 });
 
